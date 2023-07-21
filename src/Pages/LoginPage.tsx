0ed22938e8eea6b32dc5e3fb64d383c73b85fbde
David-McCaig <davidmccaig1@gmail.com>
2023-07-21 21:33:46 +1200

    Set up env variables in Firebase.tsx. Add button to LoginPage

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+
+import { useEffect, useState } from "react";
+import firebase from "firebase/app";
+import { auth } from "../utils/firebase.js";
+
+
 
 function LoginPage() {
+
+  const [user, setUser] = useState<firebase.User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((firebaseUser) => {
+      setUser(firebaseUser);
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
-    <div>LoginPage</div>
+    <div>
+      <form>
+        
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          login
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Signup
+        </button>
+
+      </form>
+    </div>
   )
 }
 
