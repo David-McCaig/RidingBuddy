8463377e422f656dd15c3e9c321c66f4da8cc5ac
David-McCaig <davidmccaig1@gmail.com>
2023-07-26 21:05:05 +1200

    Set up form validation for Login page

--- src/features/authentication/Login.tsx
+++ src/features/authentication/Login.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
-import { useFormik, FormikHelpers } from "formik"
+import { Formik, Field, Form, FormikHelpers } from "formik";
+import * as Yup from "yup"; // Import Yup for validation
 import { auth, Providers } from "../../utils/firebase.js";
 import { useDispatch } from "react-redux";
 import { login, logout, selectUser } from "./userSlice.js";
 import { useSelector } from "react-redux";
-
 
 function LoginPage() {
   const [authenticating, setAuthenticating] = useState<boolean>(false);
@@ -14,8 +14,18 @@ function LoginPage() {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
 
-  // const formRef = useRef(null);
+  const formRef = useRef(null);
 
+  interface Values {
+    password: string;
+    email: string;
+  }
+
+  // Validation schema using Yup
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    password: Yup.string().required("Password is required"),
+  });
 
   const googleSignInClick = async () => {
     if (error !== "") setError("");
@@ -29,8 +39,6 @@ function LoginPage() {
       setError((error as Error).message);
     }
   };
-
-
 
   const currentUser = auth.currentUser;
 
@@ -90,72 +98,80 @@ function LoginPage() {
             <span>Login with Google</span>
           </button>
         </div>
-        <form   className="my-10">
-          <div className="flex flex-col space-y-5">
-            <label>
-              <p className="font-medium text-slate-700 pb-2">Email address</p>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
-                placeholder="Enter email address"
-              />
-            </label>
-            <label>
-              <p className="font-medium text-slate-700 pb-2">Password</p>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
-                placeholder="Enter your password"
-              />
-            </label>
-            <div className="flex flex-row justify-between">
-              <div>
-                <label className="">
-                  <input
-                    type="checkbox"
-                    id="remember"
-                    className="w-4 h-4 mr-1 border-slate-200 focus:bg-indigo-600"
+
+        <Formik
+          initialValues={{
+            password: "",
+            email: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(
+            values: Values,
+            { setSubmitting }: FormikHelpers<Values>
+          ) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 500);
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form ref={formRef} className="my-10">
+              <div className="flex flex-col space-y-5">
+                <label>
+                  <p className="font-medium text-slate-700 pb-2">Email address</p>
+                  <Field
+                    id="email"
+                    name="email"
+                    type="email"
+                    className={`w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none ${
+                      errors.email && touched.email ? "border-red-500" : ""
+                    }`}
+                    placeholder="Enter email address"
                   />
-                  Remember me
+                  {errors.email && touched.email && (
+                    <p className="text-red-500">{errors.email}</p>
+                  )}
                 </label>
-              </div>
-              <div>
-                <a href="#" className="font-medium text-indigo-600">
-                  Forgot Password?
-                </a>
-              </div>
-            </div>
-            <button className="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-                />
-              </svg>
-              <span>Login</span>
-            </button>
-            <p className="text-center">
-              Not registered yet?{" "}
-              <Link
-                to="/signup"
-                className="text-indigo-600 font-medium inline-flex space-x-1 items-center"
-              >
-                <span>Register now </span>
-                <span>
+                <label>
+                  <p className="font-medium text-slate-700 pb-2">Password</p>
+                  <Field
+                    id="password"
+                    name="password"
+                    type="password"
+                    className={`w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none ${
+                      errors.password && touched.password ? "border-red-500" : ""
+                    }`}
+                    placeholder="Enter your password"
+                  />
+                  {errors.password && touched.password && (
+                    <p className="text-red-500">{errors.password}</p>
+                  )}
+                </label>
+                <div className="flex flex-row justify-between">
+                  <div>
+                    <label className="">
+                      <input
+                        type="checkbox"
+                        id="remember"
+                        className="w-4 h-4 mr-1 border-slate-200 focus:bg-indigo-600"
+                      />
+                      Remember me
+                    </label>
+                  </div>
+                  <div>
+                    <a href="#" className="font-medium text-indigo-600">
+                      Forgot Password?
+                    </a>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4"
+                    className="h-6 w-6"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -163,14 +179,39 @@ function LoginPage() {
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                      d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013 3h7a3 3 0 013-3v1"
                     />
                   </svg>
-                </span>
-              </Link>
-            </p>
-          </div>
-        </form>
+                  <span>Login</span>
+                </button>
+                <p className="text-center">
+                  Not registered yet?{" "}
+                  <Link
+                    to="/signup"
+                    className="text-indigo-600 font-medium inline-flex space-x-1 items-center"
+                  >
+                    <span>Register now </span>
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-4 w-4"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                        />
+                      </svg>
+                    </span>
+                  </Link>
+                </p>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
       <button onClick={signOut}>Sign Out</button>
       <p>{user?.email}</p>
@@ -179,3 +220,4 @@ function LoginPage() {
 }
 
 export default LoginPage;
+
