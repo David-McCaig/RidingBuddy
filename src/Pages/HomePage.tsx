7908733c39cb362b1f7e025bdbdbdbd2e3ad40d6
David-McCaig <davidmccaig1@gmail.com>
2023-08-23 19:50:02 +1200

    Set up like functionality in RidePosted component. Still need to clean up code

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,38 +1,43 @@
 import NavBar from "../features/navigation/Components/NavBar";
 import LoadingBar from "../Components/LoadingBar";
-import PostedRides from "../features/postedrides/index";
+import RidePosted from "../features/postedrides/Component/RidePosted";
 import ProfileCard from "../Components/ProfileCard";
 import { useSignedinDispatchUserInfo } from "../features/authentication/hooks/useSignedinDispatchUserInfo";
-import { auth } from "../utils/firebase";
+import { collection, getDocs, addDoc, query, where } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
 import { logout, selectUser } from "../features/authentication/userSlice.js";
+import { db } from "../utils/firebase";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+
+interface RideData {
+  id: string;
+  user_name?: string;
+  ride_title?: string;
+  ride_description?: string;
+}
 
 function HomePage() {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
   useSignedinDispatchUserInfo();
 
-  const signOut = () => {
-    auth
-      .signOut()
-      .then(() => {
-        console.log("Successfully signed out.");
-        dispatch(logout());
-      })
-      .catch((error) => {
-        console.error("Error signing out:", error);
-      });
-  };
+  const collectionRef = collection(db, `ridePosts`);
+  const [snapshot, loading, error] = useCollection(collectionRef);
 
-  console.log(user);
+  const posts: RideData[] =
+    snapshot?.docs.map((doc) => ({ id: doc.id, ...doc.data() })) || [];
 
   return (
     <div className="flex sm:justify-center bg-gray-50 xl:px-32 ">
-        <NavBar />
+      <NavBar />
       {/* <div className="sm:w-[15rem] lg:w-[18rem] xl:w-[100px] h-[20rem]"></div> */}
-      <div className="pt-12 ">
-        <PostedRides />
+      <div className="flex flex-col">
+        {posts?.map((post) => (
+          <div key={post.id} className="pt-12 ">
+            <RidePosted post={post} loading={loading} error={error} />
+          </div>
+        ))}
       </div>
 
       <div className="pt-[5rem]">
--- src/features/postedrides/Component/RidePosted.tsx
+++ src/features/postedrides/Component/RidePosted.tsx
@@ -2,21 +2,81 @@ import { useCollection } from "react-firebase-hooks/firestore";
 import RideComment from "./RideComment";
 import RidePostAComment from "./RidePostAComment";
 import { db } from "../../../utils/firebase";
-import { collection, addDoc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  query,
+  where,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 import LoadingBar from "../../../Components/LoadingBar";
-import { CommentOutlined, HeartOutlined } from "@ant-design/icons";
-import { useEffect } from "react";
+import { CommentOutlined, HeartOutlined, HeartFilled } from "@ant-design/icons";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../authentication/userSlice";
 
 interface RideData {
   id: string;
+  user_id: string;
   user_name?: string;
   ride_title?: string;
   ride_description?: string;
 }
 
-function RidePost() {
-  const collectionRef = collection(db, `ridePosts`);
-  const [snapshot, loading, error] = useCollection(collectionRef);
+function RidePost({ loading, error, post }: any) {
+  const loggedInUser = useSelector(selectUser);
+  const [likes, setLikes] = useState<{ userId: string }[] | null>([]);
+
+  // Extract data and document IDs from the snapshot
+
+  const likesRef = collection(db, "likes");
+
+  const PostALike = async (postId: string) => {
+    try {
+      await addDoc(likesRef, {
+        user_id: loggedInUser?.userId,
+        post_id: postId,
+      });
+      getLike();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const deleteALike = async () => {
+    const likesToDeleteQuery = query(
+      likesRef,
+      where("post_id", "==", post.id),
+      where("user_id", "==", loggedInUser.userId)
+    );
+    try {
+      const likeToDeleteData = await getDocs(likesToDeleteQuery);
+      const likeToDelete = doc(db, "likes", likeToDeleteData.docs[0].id);
+      await deleteDoc(likeToDelete);
+      getLike();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const likesDoc = query(likesRef, where("post_id", "==", post.id));
+
+  const getLike = async () => {
+    const data = await getDocs(likesDoc);
+    if (loggedInUser) {
+      setLikes(data.docs.map((doc) => ({ userId: doc.data().user_id })));
+    }
+  };
+
+  const userLike: any = loggedInUser
+    ? likes?.filter((like) => like?.userId === loggedInUser.userId)
+    : [];
+
+  useEffect(() => {
+    getLike();
+  }, []);
 
   // Handle loading and error states if needed
   if (loading) {
@@ -31,67 +91,62 @@ function RidePost() {
     return <div>Error: {error.message}</div>;
   }
 
-  if (!snapshot || snapshot.empty) {
+  if (!post || post.empty) {
     return <div>No data found.</div>;
   }
 
-
-
-  // Extract data and document IDs from the snapshot
-  const docs: RideData[] =
-    snapshot?.docs.map((doc) => ({ id: doc.id, ...doc.data() })) || [];
-
   return (
-    <>
-      <div className="">
-        {/* <div className="sm:w-72 sm:h-9 xl:w-80"></div> */}
-        <div className="flex flex-col">
-          {docs?.map((post) => (
-            <div key={post.id} className="flex justify-center mt-4 ">
-              <div className="rounded-xl border p-5 drop-shadow-sm w-9/12 xl:w-11/12 bg-white">
-                <div className="flex w-full items-center justify-between border-b pb-3">
-                  <div className="flex items-center space-x-3">
-                    <div className="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]"></div>
-                    <div className="text-lg font-bold text-slate-700">
-                      {post.user_name}
-                    </div>
-                  </div>
-                  <div className="flex items-center space-x-8">
-                    <div className="text-xs text-neutral-500">2 hours ago</div>
-                  </div>
-                </div>
+    <section key={post?.id} className="flex justify-center mt-4 ">
+      <div className="rounded-xl border p-5 drop-shadow-sm w-9/12 xl:w-11/12 bg-white">
+        <div className="flex w-full items-center justify-between border-b pb-3">
+          <div className="flex items-center space-x-3">
+            <div className="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]"></div>
+            <div className="text-lg font-bold text-slate-700">
+              {post?.user_name}
+            </div>
+          </div>
+          <div className="flex items-center space-x-8">
+            <div className="text-xs text-neutral-500">2 hours ago</div>
+          </div>
+        </div>
 
-                <div className="mt-4 mb-6">
-                  <h3 className="mb-3 text-xl font-medium md:text-2xl">
-                    {post.ride_title}
-                  </h3>
-                  <div className="text-sm md:text-base text-neutral-600">
-                    {post.ride_description}
-                  </div>
-                </div>
+        <div className="mt-4 mb-6">
+          <h3 className="mb-3 text-xl font-medium md:text-2xl">
+            {post?.ride_title}
+          </h3>
+          <div className="text-sm md:text-base text-neutral-600">
+            {post?.ride_description}
+          </div>
+        </div>
 
-                <div>
-                  <div className="flex items-center justify-between text-slate-500 md:mb-2 lg:mb-4">
-                    <div className="flex space-x-4 md:space-x-8">
-                      <div className="flex cursor-pointer items-center transition hover:text-slate-700">
-                        <CommentOutlined className="mr-2.5 mb-1 text-lg " />
-                        <span>125</span>
-                      </div>
-                      <div className="flex cursor-pointer items-center transition hover:text-slate-700 ">
-                        <HeartOutlined className="mr-2.5 mb-1 text-lg " />
-                        <span>4</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <RideComment path={`ridePosts/${post.id}/comments`} />
-                <RidePostAComment id={post.id} />
+        <div>
+          <div className="flex items-center justify-between text-slate-500 md:mb-2 lg:mb-4">
+            <div className="flex space-x-4 md:space-x-8">
+              <div className="flex cursor-pointer items-center transition hover:text-slate-700">
+                <CommentOutlined className="mr-2.5 mb-1 text-lg " />
+                <span>125</span>
+              </div>
+              <div className="flex cursor-pointer items-center transition hover:text-slate-700 ">
+                {userLike?.length > 0 ? (
+                  <HeartFilled
+                    onClick={deleteALike}
+                    className="mr-2.5 mb-1 text-lg text-red-600 "
+                  />
+                ) : (
+                  <HeartOutlined
+                    onClick={() => PostALike(post.id)}
+                    className="mr-2.5 mb-1 text-lg  "
+                  />
+                )}
+                <span>{likes?.length}</span>
               </div>
             </div>
-          ))}
+          </div>
         </div>
+        <RideComment path={`ridePosts/${post?.id}/comments`} />
+        <RidePostAComment id={post?.id} />
       </div>
-    </>
+    </section>
   );
 }
 
